d8c72deaf9e975a25e2bf1673d13823319f1efaf
wdrayton <wdrayton@live.unc.edu>
2023-05-05 12:11:53 -0400

    Second submission

--- public/scripts.js
+++ public/scripts.js
@@ -1,32 +1,49 @@
-// If you would like to see some examples of similar code to make an interface interact with an API, 
-// check out the coin-server example from a previous COMP 426 semester.
-// https://github.com/jdmar3/coinserver
-
+// This function shows and hides the shot selection in the interface based 
+// on whether or not the #opponent checkbox is checked
 function showHideShots() {
-    let check = document.getElementById('opponent');
-    let radiorps = document.getElementsByClassName('rps');
-    let radiorpsls = document.getElementsByClassName('rpsls');
-
-    if (check.checked == true) {
-        $('.shots').show();
-    } else {
-        $('.shots').hide();
+    // Get the info from the checkbox
+          let check = document.getElementById('opponent');
+    // Check if the checkbox is checked and show or hide options accordingly
+        if (check.checked == true) {
+    // Here, instead of just showing all of the options, use similar logic to 
+    // check which of the game radio buttons is checked and show only those
+    // options relevant to the game being selected (rps or rpsls). You can 
+    // use similar jQuery 
+            $('.shots').show()
+        } else {
+            $('.shots').hide()
+        }
     }
-}
-
-function startOver() {
-    document.getElementById('userinput').reset();
-    showHideShots();
-}
-
-async function playGame() {
-    let game = $('input[type=radio][name=game]:checked').val();
-    let shot = $('input[type=radio][name=shot]:checked').val();
-
-    let baseURL = window.location.href + 'app/';
-    let url = baseURL + game + '/play/';
-
-    let response = await fetch(url); //Insert URL for A04 API here
-    let result = await response.json();
-
-}
+    // This function clears the input form and also resets the shot selection
+    // radio buttons. 
+    function startOver () {
+        document.getElementById('userinput').reset();
+        showHideShots();
+    }
+    
+    async function playGame () {
+        // Get which game is being played based on the value in the form
+        let game = $('input[type=radio][name=game]:checked').val();
+        // Get which shot is being played based on the value in the form
+        let shot = $('input[type=radio][name=shot]:checked').val();
+        // Identify the base URL based on browser information
+        let baseurl = window.location.href + 'app/'
+        // Log the base URL
+        console.log(baseurl)
+        // This constructs a URL for the opponent option ONLY. To incorporate
+        // the other option, you can use a conditional to change the URL based
+        // on what is selected. You could also write separate functions, or use
+        // a conditional somewhere above in this function to construct the 
+        // correct URL
+        let url = baseurl + game + '/play/' + shot
+        // Log the full URL
+        console.log(url)	
+    
+        let response = await fetch(url)
+        let result = await response.json()
+        // Log the result
+        console.log(result)
+        // Here you should include code that uses the DOM API or jQuery to 
+        // manipulate another block of HTML in the interface to display the 
+        // results in some way. 
+    }
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-//// Load most basic dependencies
+import minimist from 'minimist';
+import {playRPS} from "./lib/rpsls.js";
+import {playRPSLS} from "./lib/rpsls.js";
+import express from 'express';
 // Create require function 
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
 import { createRequire } from 'node:module';
@@ -102,3 +105,71 @@ process.on('SIGINT', () => {
         }    
     })
 })
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/app/', (req, res) => {
+    res.statusCode = 200;
+    res.send("200 OK");
+    res.end();
+});
+
+//RPS games
+app.get('/app/rps/', (req, res) => {
+    res.send(playRPS(''));
+    res.end();
+});
+
+app.get('/app/rps/play/', (req, res) => {
+    const shot = req.query.shot;
+    res.send(JSON.parse(playRPS(shot)));
+    res.end();
+});
+
+app.post('/app/rps/play/', (req, res) => {
+    const shot = req.body.shot;
+    res.send(JSON.parse(playRPS(shot)));
+    res.end();
+});
+
+app.get('/app/rps/play/:shot', (req, res) => {
+    const shot = req.params.shot;
+    res.send(JSON.parse(playRPS(shot)));
+    res.end();
+});
+
+//RPSLS games
+app.get('/app/rpsls/', (req, res) => {
+    res.send(playRPSLS(''));
+    res.end();
+})
+
+app.get('/app/rpsls/play/', (req, res) => {
+    const shot = req.query.shot;
+    res.send(JSON.parse(playRPSLS(shot)));
+    res.end();
+});
+
+app.post('/app/rpsls/play/', (req, res) => {
+    const shot = req.body.shot;
+    res.send(JSON.parse(playRPSLS(shot)));
+    res.end();
+});
+
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    const shot = req.params.shot;
+    res.send(JSON.parse(playRPSLS(shot)));
+    res.end();
+});
+
+//Error
+app.use((req, res) => {
+    res.statusCode = 404;
+    res.send("404 NOT FOUND");
+});
+
+app.listen(port, () => {
+    console.log("Server running on port " + port);
+});
